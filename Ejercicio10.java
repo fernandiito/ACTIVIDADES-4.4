0d5aa83bd0f967047601208dfb3fdaed0bd0a6b5
fernandiito <150377751+fernandiito@users.noreply.github.com>
2023-12-11 20:28:22 +0100

    Update Ejercicio10.java

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -11,33 +11,42 @@
  */
 public class Ejercicio10 {
     public static void main(String[] args) {
-        float suamtorioAlturas = 0, altMedia = 0, altMax = 0, altMin = 99999;
+       float sumatorioAlturas = 0, altMedia = 0, altMax = 0, altMin = 99999;
         int contEncima = 0, contDebajo = 0;
 
-        int N = Integer.parseInt(System.console().readLine("A cuantas personas vas a tomar la altura: "));
-        int[] num = new int[N];
-        /**
-         * El bucle primero pide la altura de la persona, actualiza las alturas minimas
-         * y maximas, calcula la altura media y aumenta el contador en funcion de si la
-         * persona mide por encima o por debajo de la media
-         */
-        for (int i = 0; i < num.length; i++) {
-            float altura = Float.parseFloat(System.console().readLine("Introduce la altura de la persona " + i + ": "));
+        int N = Integer.parseInt(System.console().readLine("A cuántas personas vas a tomar la altura: "));
+        // usar un nombre para el array mas descriptivo
+        float[] alturas = new float[N];
+
+        for (int i = 0; i < alturas.length; i++) {
+            alturas[i] = Float.parseFloat(System.console().readLine("Introduce la altura de la persona " + i + ": "));
+        }
+
+        // Calcular la suma, media, máxima y mínima
+        for (int i = 0; i < alturas.length; i++) {
+            float altura = alturas[i];
+            sumatorioAlturas = sumatorioAlturas + altura;
+
             if (altMin > altura) {
                 altMin = altura;
-            } else if (altMax < altura) {
+            }
+            if (altMax < altura) {
                 altMax = altura;
             }
-            suamtorioAlturas = suamtorioAlturas + altura;
-            altMedia = suamtorioAlturas / N;
-            if (altura > altMedia) {
+        }
+
+        altMedia = sumatorioAlturas / N;
+
+        // Contar personas encima/debajo de la media
+        for (int i = 0; i < alturas.length; i++) {
+            float altura = alturas[i];
+            if (altura >= altMedia) {
                 contEncima++;
-            } else {
+            } else if (altura < altMedia) {
                 contDebajo++;
             }
         }
 
-        // altMedia=suamtorioAlturas/N;
         System.out.println("La altura media es: " + altMedia);
         System.out.println("La altura máxima es: " + altMax);
         System.out.println("La altura mínima es: " + altMin);
